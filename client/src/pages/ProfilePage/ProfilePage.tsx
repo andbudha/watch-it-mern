356baa4cdc76d43c5fe26148a64af28fb8f9b07d
andbudha <andreibartov@gmail.com>
2024-06-24 12:26:05 +0200

    ProfilePage created

---
+++ client/src/pages/ProfilePage/ProfilePage.tsx
@@ -0,0 +1,9 @@
+import styles from './ProfilePage.module.scss';
+
+export const ProfilePage = () => {
+  return (
+    <div className={styles.profilepage_main_box}>
+      <h1>Profile Page</h1>
+    </div>
+  );
+};
