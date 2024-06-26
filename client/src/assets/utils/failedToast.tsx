93f8ebd91996487134e7324d431d793b1ddfc499
andbudha <andreibartov@gmail.com>
2024-06-26 16:00:03 +0200

    error toast creating func updated

--- client/src/assets/utils/failedToast.tsx
+++ client/src/assets/utils/failedToast.tsx
@@ -1,4 +1,3 @@
-import { FirebaseError } from 'firebase/app';
 import toast from 'react-hot-toast';
 
 export const toastError = (message: string) =>
@@ -15,20 +14,3 @@ export const toastError = (message: string) =>
       secondary: '#fff',
     },
   });
-
-export const generateFirebaseErrorInstance = (error: FirebaseError) => {
-  switch (error?.code) {
-    case 'auth/invalid-credential':
-      toastError('Either email or password is incorrect. Try again, please.');
-      break;
-    case 'auth/email-already-in-use':
-      toastError('The chosen email is already in use. Pick another email.');
-      break;
-    case 'auth/network-request-failed':
-      toastError('Network request failure. Try again, please.');
-      break;
-    default:
-      toastError('Some error occurred. Try again, please.');
-      break;
-  }
-};
