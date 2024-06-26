803db83e67cb1bb736c2c6be449e8fa9473e0b43
andbudha <andreibartov@gmail.com>
2024-06-26 15:58:34 +0200

    commentarySchema created and applied

--- server/models/movieModel.js
+++ server/models/movieModel.js
@@ -1,12 +1,19 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
-
+const commentarySchema = new Schema({
+  commentaryID: { type: String, required: true, unique: true },
+  movieID: { type: String, required: true },
+  userID: { type: String, required: true },
+  avatar: { type: String },
+  nickName: { type: String },
+  commentary: { type: String, required: true },
+});
 const movieSchema = new Schema({
   title: { type: String },
   year: { type: Number },
   cast: [{ type: String }],
   genres: [{ type: String }],
-  commentaries: [{ type: Schema.Types.Mixed }, { timestamps: true }],
+  commentaries: [commentarySchema, { timestamps: true }],
   href: { type: String },
   extract: { type: String },
   thumbnail: { type: String },
