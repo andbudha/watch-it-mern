6f13a2f9109c253203fee9fa3f9f7021948f2a61
andbudha <andreibartov@gmail.com>
2024-07-04 11:33:18 +0200

    Ratings displayed in MovieCard

--- client/src/components/MovieCard/MovieCard.tsx
+++ client/src/components/MovieCard/MovieCard.tsx
@@ -2,6 +2,7 @@ import { NavLink } from 'react-router-dom';
 import { Movie } from '../../types/common_types';
 import styles from './MovieCard.module.scss';
 import { BiCameraMovie } from 'react-icons/bi';
+import { IoMdThumbsUp, IoMdThumbsDown } from 'react-icons/io';
 
 type MovieCardProps = {
   movie: Movie;
@@ -33,6 +34,24 @@ export const MovieCard = ({ movie }: MovieCardProps) => {
           </div>
         )}
       </NavLink>
+      <div className={styles.rating_main_box}>
+        <div className={styles.rating_box}>
+          <div className={styles.thumb_box}>
+            {' '}
+            <IoMdThumbsUp className={styles.thumb_icon} />
+          </div>
+          <div className={styles.total_thumbs_box}>{movie.likes?.length}</div>
+        </div>
+        <div className={styles.rating_box}>
+          <div className={styles.thumb_box}>
+            {' '}
+            <IoMdThumbsDown className={styles.thumb_icon} />
+          </div>
+          <div className={styles.total_thumbs_box}>
+            {movie.dislikes?.length}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
