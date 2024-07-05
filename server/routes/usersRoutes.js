a3cfd7a8fa16d09aa9d407d29b2dc3070fef8971
andbudha <andreibartov@gmail.com>
2024-07-05 13:03:41 +0200

    multerUpload middleware func created&applied

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -6,12 +6,13 @@ import {
   updateProfile,
 } from '../controllers/usersController.js';
 import JWTAuth from '../middlewares/JWTAuth.js';
+import { multerUpload } from '../middlewares/multer.js';
 
 const router = express.Router();
 
 router.post('/register', registerNewUser);
 router.post('/login', userLogin);
-router.post('/updateprofile', updateProfile);
+router.post('/updateprofile', multerUpload.single('avatar'), updateProfile);
 router.get('/profile', JWTAuth, getUserProfile);
 
 export default router;
