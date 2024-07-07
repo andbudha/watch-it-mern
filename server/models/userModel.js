4f611ab21fb59c88d2eac7c404eddf2522af60f6
andbudha <andreibartov@gmail.com>
2024-07-07 11:49:47 +0200

    userModel updated/versionKey set to false

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,14 +1,19 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  nickName: { type: String, required: true, unique: true },
-  avatar: { type: String },
-  avatarPublicID: { type: String },
-  movieList: [{ type: Schema.Types.ObjectId, ref: 'movie' }],
-});
+const userSchema = new Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    nickName: { type: String, required: true, unique: true },
+    avatar: { type: String },
+    avatarPublicID: { type: String },
+    movieList: [{ type: Schema.Types.ObjectId, ref: 'movie' }],
+  },
+  {
+    versionKey: false,
+  }
+);
 
 const UserModel = mongoose.model('user', userSchema);
 export default UserModel;
